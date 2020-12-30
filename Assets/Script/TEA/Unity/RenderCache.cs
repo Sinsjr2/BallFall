741f961677395232d0393e2e18918fd5c846e52f
Sinsjr2 <hks15sirubia@gmail.com>
2020-12-30 23:33:17 +0900

    RenderCacheクラスとMonoBehaviourRenderFactoryクラスを別ファイルに移動しました。

---
+++ Assets/Script/TEA/Unity/RenderCache.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+/// <summary>
+///   前回書き込んだ状態と比較して、異なっていればRenderを呼び出すようにします。
+/// </summary>
+[Serializable]
+public struct RenderCache<T, Input, State, Act> : IRender<Input, State, Act>
+    where T : class, IRender<Input, State, Act>
+    where State : IEquatable<State> {
+
+    [SerializeField]
+    T render;
+
+    /// <summary>
+    ///   前回Renderに渡した値
+    /// </summary>
+    State prevState;
+
+    public T GetRender() {
+        Assert.IsNotNull(render);
+        return render;
+    }
+
+    public void Setup(Input input, IDispacher<Act> dispacher) {
+        Assert.IsNotNull(render);
+        render.Setup(input, dispacher);
+    }
+
+    public void Render(State state) {
+        if (prevState.Equals(state)) {
+            return;
+        }
+        prevState = state;
+        render.Render(state);
+    }
+}
