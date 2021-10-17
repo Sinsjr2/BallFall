9520476369d5b6a3f8161035f357ea367e518674
Sinsjr2 <hks15sirubia@gmail.com>
2021-10-17 19:27:12 +0900

    StateInitializerインターフェスを使用していないので削除しました。

--- Assets/Script/TEA/StateInitializer.cs
+++
@@ -1,9 +0,0 @@
-namespace TEA {
-    public interface StateInitializer<Input, State> {
-        /// <summary>
-        ///   状態の作成を行います。
-        ///   Unityであるとインスペクターから設定した値を初期値とする場合が多いのでこのようにしています。
-        /// </summary>
-        State CreateState(Input initial);
-    }
-}
