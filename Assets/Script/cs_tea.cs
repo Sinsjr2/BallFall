c47cb880f5ab7a52697f86cd2650607e3462692e
Sinsjr2 <hks15sirubia@gmail.com>
2021-10-17 19:11:33 +0900

    使用していないNoneUpdateを削除しました。

--- Assets/Script/cs_tea.cs
+++ Assets/Script/cs_tea.cs
@@ -1,17 +1,4 @@
 using System;
-using TEA;
-
-
-/// <summary>
-///   何も行いません。
-/// </summary>
-public class NoneUpdate<State, Msg> : IUpdate<State, Msg> {
-    public static readonly NoneUpdate<State, Msg> Instance = new NoneUpdate<State, Msg>();
-    private NoneUpdate() {}
-    public State Update(State state, Msg msg) {
-        return state;
-    }
-}
 
 public enum Unit {
     Default
