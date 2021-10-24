6c908768eed7c9889991406e1f4cf59a888f63eb
Sinsjr2 <hks15sirubia@gmail.com>
2021-10-24 11:26:04 +0900

    使用しておらず、ラムダ式を使えば不要であるので削除しました。

--- Assets/Script/TEA/MessageWrapper.cs
+++ Assets/Script/TEA/MessageWrapper.cs
@@ -1,21 +1,6 @@
 using System;
 
 namespace TEA {
-    public class MessageWrapper<Input, Before, After> : IDispatcher<After> {
-
-        readonly IDispatcher<Before> dispatcher;
-        readonly Action<IDispatcher<Before>, Input, After> dispatch;
-        public Input value { set; private get; }
-
-        public MessageWrapper(IDispatcher<Before> dispatcher, Action<IDispatcher<Before>, Input, After> dispatch) {
-            this.dispatcher = dispatcher;
-            this.dispatch = dispatch;
-        }
-
-        public void Dispatch(After msg) {
-            dispatch(dispatcher, value, msg);
-        }
-    }
 
     public class MessageWrapper<TSource, TResult> : IDispatcher<TSource> {
 
@@ -37,12 +22,6 @@ namespace TEA {
             this IDispatcher<TResult> dispatcher,
             Func<TSource, TResult> selector) {
             return new MessageWrapper<TSource, TResult>(dispatcher, selector);
-        }
-
-        public static MessageWrapper<Input, Before, After> Wrap<Input, Before, After>(
-            this IDispatcher<Before> dispatcher,
-            Action<IDispatcher<Before>, Input, After> dispatch) {
-            return new MessageWrapper<Input, Before, After>(dispatcher, dispatch);
         }
     }
 }
